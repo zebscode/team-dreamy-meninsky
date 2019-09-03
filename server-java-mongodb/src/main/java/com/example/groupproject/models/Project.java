ad572175643fc5101157d523b0f41cf336af3890
Ammon King <ammon.king@woz-u.com>
2019-09-03 09:18:48 -0700

    added base project structure

---
+++ server-java-mongodb/src/main/java/com/example/groupproject/models/Project.java
@@ -0,0 +1,36 @@
+package com.example.groupproject.models;
+
+import org.springframework.data.annotation.Id;
+
+public class Project {
+
+  @Id
+  private String id;
+  private String name;
+  private String createdBy;
+
+  public String getId() {
+    return this.id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCreatedBy() {
+    return this.createdBy;
+  }
+
+  public void setCreatedBy(String createdBy) {
+    this.createdBy = createdBy;
+  }
+
+}

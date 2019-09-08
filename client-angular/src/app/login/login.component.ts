0483cd857568866ba68358bc21d52aa864f125b2
Andrea Dixon <nicole021984@sbcglobal.net>
2019-09-08 08:27:26 -0700

    initial commit

--- client-angular/src/app/login/login.component.ts
+++ client-angular/src/app/login/login.component.ts
@@ -1,28 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { User } from './models/user';
+import { User } from '../models/user';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-
-export class AppComponent {
+export class LoginComponent implements OnInit {
   title: string = 'team-dreamy-meninsky';
   token: string = '';
   user: User = new User()
   error: string;
   base_url: string = "http://localhost:8080/";
   data: any;
-  
+
   constructor(private http: HttpClient) { }
+
+  ngOnInit() {}
 
   onLogin() {
     this.http.post<any>(this.base_url + 'login', { username: this.user.username, password: this.user.password }, { observe: 'response' })
     .subscribe(
       res => this.token = res.headers.get("Authorization"),
-      error => this.error = "Unbale to login with username and password.");
+      error => this.error = "Unable to login with username and password.");
     }
     getValues() {
       let headers: HttpHeaders = new HttpHeaders();
@@ -30,4 +31,4 @@ export class AppComponent {
       this.http.get(this.base_url + "api/values", { headers: headers })
         .subscribe(res => this.data = res, error => this.error = "Unable to retrieve data.");
     }
-}
+  }

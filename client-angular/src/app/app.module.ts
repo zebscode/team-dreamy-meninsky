4247843dccaf8d42270bf74e47380732d1b05281
Andrea Dixon <nicole021984@sbcglobal.net>
2019-09-12 10:34:04 -0700

    latest commit

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
@@ -9,6 +10,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ProjectDisplayComponent } from './project-display/project-display.component';
 import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [AppComponent, ProjectDisplayComponent, LoginComponent],

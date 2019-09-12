0b4d6101a5bd88412cc43602f853a98279636b3e
Zebulon <Donnell>
2019-09-12 18:38:09 -0500

    adding java api

---
+++ server-java-mongodb/src/main/java/com/example/groupproject/auth/User.java
@@ -0,0 +1,39 @@
+package com.example.groupproject.auth;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long userId;
+	
+	@Column(nullable=false, unique=true)
+	private String username;
+	private String password;
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}

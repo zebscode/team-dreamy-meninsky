b3ebf18b7e710a1c1a8233ccbc480c239b34134e
ljwhitt83 <ljwhitt83@gmail.com>
2019-09-29 19:40:35 -0700

    added bootstrap style to login and registration

--- client-angular/src/app/user.service.ts
+++ client-angular/src/app/user.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { User } from "./user";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from './user';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class UserService {
   constructor(private http: HttpClient) {}
@@ -15,46 +15,46 @@ export class UserService {
 
   // base url of the express back end
   // Ammon: Java! runs on port 8080
-  url: string = "http://localhost:8080/";
+  url: string = 'http://localhost:8080/';
 
   // boolean value to hold the login status
   loggedIn: boolean = false;
 
   private authHeader() {
-    let token = localStorage.getItem("jtw");
+    const token = localStorage.getItem('jtw');
     let headers: HttpHeaders = new HttpHeaders();
-    headers = headers.append("Authorization", token);
+    headers = headers.append('Authorization', token);
     return headers;
   }
 
   // register a user, must .subscribe() to trigger POST baseurl/signup
   registerUser(user: User): Observable<string> {
-    return this.http.post<string>(this.url + "register", user);
+    return this.http.post<string>(this.url + 'register', user);
   }
 
   // login a user, must .subscribe() to trigger POST baseurl/login
   // Ammon: return type any, we're not really returning any data besides the response which is a complex object
   loginUser(user: User): any {
     // Ammon: we set the observe : response property so that we can listen to the response headers
-    return this.http.post<string>(this.url + "login", user, { observe: 'response' });
+    return this.http.post<string>(this.url + 'login', user, { observe: 'response' });
   }
 
   // get a user profile, must .subscribe() to trigger GET baseurl/profile
   getProfile(): Observable<User> {
-    return this.http.get<User>(this.url + "profile",{headers: this.authHeader()});
+    return this.http.get<User>(this.url + 'profile', {headers: this.authHeader()});
   }
 
   // logout, must .subscribe() to trigger GET baseurl/logout
   logout(): Observable<string> {
-    return this.http.get<string>(this.url + "logout", this.options);
+    return this.http.get<string>(this.url + 'logout', this.options);
   }
 
   // validate a token, must .subscribe() to trigger GET baseurl/validateToken
   validateToken(): Observable<boolean> {
-    return this.http.get<boolean>(this.url + "validateToken", this.options);
+    return this.http.get<boolean>(this.url + 'validateToken', this.options);
   }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.url + "users", this.options);
+    return this.http.get<User[]>(this.url + 'users', this.options);
   }
 }
